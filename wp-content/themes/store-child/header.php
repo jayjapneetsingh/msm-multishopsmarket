de54c7c136c8a9fa7a28126ece5b1e149721e008
jay <jay.japneet@rubicotech.in>
2022-10-04 18:28:51 +0530

    wip: lit-bit change

--- wp-content/themes/store-child/header.php
+++ wp-content/themes/store-child/header.php
@@ -8,12 +8,12 @@
 *
 * @package jay
 */
- 
+
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-   <meta charset="<?php bloginfo( 'charset' ); ?>">
+   <meta charset="<?php bloginfo('charset'); ?>">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="profile" href="https://gmpg.org/xfn/11">
  
@@ -22,46 +22,45 @@
  
 <body <?php body_class(); ?>>
 
-
-
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo esc_url( home_url('/')) ;?>">
-    <?php if (function_exists('the_custom_logo')){
-                the_custom_logo();
-            }
-            ?> </a>
-            <?php get_search_form( ); ?>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <div class="container-fluid">
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')) ;?>">
+    <?php if (function_exists('the_custom_logo')) {
+        the_custom_logo();
+    }
+?> </a>
 
     <div class="collapse navbar-collapse" id="navbarNav">
-    <?php 
-       
-      
-       $header_menu = array(
-     'theme_location'  => 'header_menus',
-     'menu_class' => 'navbar-nav',
-     'li_class' => 'nav-item',
-     'a_class' => 'nav-link',
-     'active_class' => 'active',
+    
+    <?php
 
-  );
- wp_nav_menu($header_menu);
+      $header_menu = array(
+        'theme_location'  => 'header_menus',
+        'menu_class' => 'navbar-nav',
+        'li_class' => 'nav-item',
+        'a_class' => 'nav-link',
+        'active_class' => 'active',
 
-?>
-  
+      );
+      wp_nav_menu($header_menu);
+
+    ?>
+
+<?php get_search_form(); ?>
+
      <?php $login_menu = array(
-     'theme_location'  => 'login_menus',
-     'container'       => '',
-     'menu_class' => '',
-     'li_class' => '',
-     'a_class' => 'nav-link',
-     'active_class' => 'active',
-     'container_class' => '',
-     'container_id'    => '',
+        'theme_location'  => 'login_menus',
+        'container'       => '',
+        'menu_class' => '',
+        'li_class' => '',
+        'a_class' => 'nav-link',
+        'active_class' => 'active',
+        'container_class' => '',
+        'container_id'    => '',
+      );
 
-  );
- wp_nav_menu($login_menu);
+  wp_nav_menu($login_menu);
 
 ?>
       
