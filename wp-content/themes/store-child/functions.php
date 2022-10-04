12c9bdc49e64e4fcc39ef9ddf170eaa6f5394069
jay <jay.japneet@rubicotech.in>
2022-10-04 15:28:17 +0530

    wip: adding logo and nav menus

--- wp-content/themes/store-child/functions.php
+++ wp-content/themes/store-child/functions.php
@@ -14,3 +14,36 @@ function my_theme_enqueue_styles() {
 }
 
 //single line comment
+
+
+
+function themename_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true, 
+    );
+ 
+    add_theme_support( 'custom-logo', $defaults );
+}
+ 
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+
+
+// Register Nav Menus 
+
+function msm_nav_menus(){
+    register_nav_menus(array(
+        'header-menu' =>__("Header Menu", 'salesvue_nav_menus'),
+        'login-menu' => __("Login Menu",'salesvue_nav_menus' ),
+        
+            ) );
+        
+
+
+}
+
+add_action('init', 'msm_nav_menus');
