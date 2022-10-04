c2b661bc3c088d8d51670ac89186518849ca4c7c
jay <jay.japneet@rubicotech.in>
2022-10-04 18:30:24 +0530

    wip: Adding footer-sidebar

---
+++ wp-content/themes/store-child/inc/footer-sidebar.php
@@ -0,0 +1,34 @@
+<?php
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+
+        'name' => 'Footer column 1',
+        'id' => 'footer-column-1',
+        'description' => 'Content of First footer Widget',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>',
+    ));
+
+    register_sidebar(array(
+        'name' => 'Footer column 2',
+        'id' => 'footer-column-2',
+        'description' => 'Content of Second footer Widget',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>',
+    ));
+      register_sidebar(array(
+        'name' => 'Footer column 3',
+        'id' => 'footer-column-3',
+        'description' => 'Content of Third footer Widget',
+        'before_widget' => '<div >',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>',
+    ));
+
+}
+?>
