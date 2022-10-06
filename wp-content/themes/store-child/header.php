ebaecb8d2fa2d81ba40fb8e6e2e86a34bd621957
jay <jay.japneet@rubicotech.in>
2022-10-06 12:44:13 +0530

    wip: Adding nav menus custom logo and searchbar

--- wp-content/themes/store-child/header.php
+++ wp-content/themes/store-child/header.php
@@ -13,7 +13,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="https://gmpg.org/xfn/11">
   <?php wp_head(); ?>
@@ -23,27 +23,27 @@
 <!-- Body Start -->
 <body <?php body_class(); ?>>
   <?php
-   $custom_logo_id = get_theme_mod( 'custom_logo' );
-   $image = wp_get_attachment_image_src( $custom_logo_id , 'medium' );
-  ?>
+   $custom_logo_id = get_theme_mod('custom_logo');
+$image = wp_get_attachment_image_src($custom_logo_id, 'medium');
+?>
   <header>
     <nav class="custom-navbar d-flex ">
       <div class="container-fluid custom-container d-flex align-items-center">
-        <a class="navbar-brand" href="<?php echo esc_url( home_url('/')) ;?>"><img src="<?php echo $image[0];?>"></a>
+        <a class="navbar-brand" href="<?php echo esc_url(home_url('/')) ;?>"><img src="<?php echo $image[0];?>"></a>
         <div class="collapse navbar-collapse show" id="navbarNav">
           <div class="nav-menu d-flex align-items-center justify-content-evenly">
             
           <!--This is header nav menus -->
 
             <?php
-                $header_menu = array(
-                'theme_location'  => 'header_menus',
-                'menu_class' => 'navbar-nav d-flex align-items-center ms-0',
-                'li_class' => 'nav-item',
-                'a_class' => 'nav-link',
-                'active_class' => 'active',
-                );
-               wp_nav_menu($header_menu);
+              $header_menu = array(
+              'theme_location'  => 'header_menus',
+              'menu_class' => 'navbar-nav d-flex align-items-center ms-0',
+              'li_class' => 'nav-item',
+              'a_class' => 'nav-link',
+              'active_class' => 'active',
+              );
+              wp_nav_menu($header_menu);
             ?>
             <!-- Header nav menus end -->
 
@@ -59,16 +59,12 @@
 
             <!-- This is a second nav menu called as Login menu -->
             <?php $login_menu = array(
-                  'theme_location'  => 'login_menus',
-                  'container'       => '',
-                  'menu_class' => 'btn-menu m-0',
-                  'li_class' => '',
-                  'a_class' => 'nav-link',
-                  'active_class' => 'active',
-                  'container_class' => '',
-                  'container_id'    => '',
-                );
-                wp_nav_menu($login_menu);
+             'theme_location'  => 'login_menus',
+              'menu_class' => 'btn-menu m-0',
+              'a_class' => 'nav-link',
+              'active_class' => 'active',
+              );
+              wp_nav_menu($login_menu);
             ?>
 
           </div>
