e7c1eb479eb0736a5a1f913b74dcbe854c88b854
jay <jay.japneet@rubicotech.in>
2022-10-06 12:43:17 +0530

    wip: Adding nav menus custom logo and searchbar

--- wp-content/themes/store-child/header.php
+++ wp-content/themes/store-child/header.php
@@ -13,59 +13,67 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-   <meta charset="<?php bloginfo('charset'); ?>">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="profile" href="https://gmpg.org/xfn/11">
- 
-   <?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+  <?php wp_head(); ?>
 </head>
- 
+<!-- Head end -->
+
+<!-- Body Start -->
 <body <?php body_class(); ?>>
+  <?php
+   $custom_logo_id = get_theme_mod( 'custom_logo' );
+   $image = wp_get_attachment_image_src( $custom_logo_id , 'medium' );
+  ?>
+  <header>
+    <nav class="custom-navbar d-flex ">
+      <div class="container-fluid custom-container d-flex align-items-center">
+        <a class="navbar-brand" href="<?php echo esc_url( home_url('/')) ;?>"><img src="<?php echo $image[0];?>"></a>
+        <div class="collapse navbar-collapse show" id="navbarNav">
+          <div class="nav-menu d-flex align-items-center justify-content-evenly">
+            
+          <!--This is header nav menus -->
 
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')) ;?>">
-    <?php if (function_exists('the_custom_logo')) {
-        the_custom_logo();
-    }
-?> </a>
+            <?php
+                $header_menu = array(
+                'theme_location'  => 'header_menus',
+                'menu_class' => 'navbar-nav d-flex align-items-center ms-0',
+                'li_class' => 'nav-item',
+                'a_class' => 'nav-link',
+                'active_class' => 'active',
+                );
+               wp_nav_menu($header_menu);
+            ?>
+            <!-- Header nav menus end -->
 
-    <div class="collapse navbar-collapse" id="navbarNav">
-    
-    <?php
+            <!-- Search Bar -->
+            <form action="/" method="get" class="search-bar">
+              <div class="form-group">
+                <input type="text" name="s" id="search" placeholder="Search products"
+                  value="<?php the_search_query(); ?>" />
+                <img src="<?php echo get_stylesheet_directory_uri().'/Search.svg'?>" alt="">
+              </div>
+            </form>
+            <!-- Search bar end  -->
 
-      $header_menu = array(
-        'theme_location'  => 'header_menus',
-        'menu_class' => 'navbar-nav',
-        'li_class' => 'nav-item',
-        'a_class' => 'nav-link',
-        'active_class' => 'active',
+            <!-- This is a second nav menu called as Login menu -->
+            <?php $login_menu = array(
+                  'theme_location'  => 'login_menus',
+                  'container'       => '',
+                  'menu_class' => 'btn-menu m-0',
+                  'li_class' => '',
+                  'a_class' => 'nav-link',
+                  'active_class' => 'active',
+                  'container_class' => '',
+                  'container_id'    => '',
+                );
+                wp_nav_menu($login_menu);
+            ?>
 
-      );
-      wp_nav_menu($header_menu);
-
-    ?>
-
-<?php get_search_form(); ?>
-
-     <?php $login_menu = array(
-        'theme_location'  => 'login_menus',
-        'container'       => '',
-        'menu_class' => '',
-        'li_class' => '',
-        'a_class' => 'nav-link',
-        'active_class' => 'active',
-        'container_class' => '',
-        'container_id'    => '',
-      );
-
-  wp_nav_menu($login_menu);
-
-?>
-      
-    
-    </div>
-  </div>
-</nav> 
-</header>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- Header end -->
