95a3e1f0737c89cda505b6bee0ac21d757ce07d5
jay <jay.japneet@rubicotech.in>
2022-10-06 12:39:03 +0530

    wip: adding code for woocommere and register nav menus & others

--- wp-content/themes/store-child/functions.php
+++ wp-content/themes/store-child/functions.php
@@ -26,6 +26,11 @@ function my_theme_enqueue_styles() {
         array( $parenthandle ),
         $theme->get('Version') // this only works if you have Version in the style header
     );
+    wp_enqueue_style('swiper-css',"https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css");
+    wp_enqueue_script('swiper-js',"https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js",array(),false,true);
+    wp_enqueue_script('bootstrap-js',"//code.jquery.com/jquery-3.3.1.slim.min.js ",array('jquery'),true);
+    wp_enqueue_script('custom-swiper-js',get_stylesheet_directory_uri( ).'/slider.js',array(),false,true);
+
 }
 
 /* 
@@ -39,12 +44,12 @@ function msm_custom_logo() {
         'flex-height'          => true,
         'flex-width'           => true,
         'header-text'          => array( 'site-title', 'site-description' ),
-        'unlink-homepage-logo' => true, 
+        'unlink-homepage-logo' => true,
     );
-    
-}
-    add_theme_support( 'custom-logo', $defaults );
- 
+
+
+    add_theme_support('custom-logo', $defaults);
+} 
 
 
 // Register Nav Menus 
@@ -59,3 +64,57 @@ function msm_nav_menus(){
         
     }
 
+
+/*
+* This code is used for inherting the li and nav classes in nav menus use the wp_nav menus classes 
+*/
+     
+function add_class_li($classes, $item, $args){
+    if(isset($args->li_class)){
+        $classes[] =$args->li_class;
+  
+    }
+    if(isset($args ->active_class)&& in_array('current-menu-item', $classes)){
+        $classes[] =$args->active_class;
+    }
+    return $classes;
+ }
+ add_filter('nav_menu_css_class', 'add_class_li', 10,3 );
+  
+  
+
+/*
+  This code is used for inherting anchor classes <a> tag classes 
+*/
+
+ function add_anchor_classes($attr,$item,$args){
+    if(isset($args->a_class)){
+        $attr['class'] = $args->a_class;
+    }
+    return $attr;
+ }
+ add_filter('nav_menu_link_attributes', 'add_anchor_classes', 10,3);
+ 
+
+
+/*
+ This function enable woocomerce support for developers 
+*/
+
+
+ function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+/*
+* This code is used for showing product when no post found on search bar
+*/
+
+
+add_action( 'woocommerce_no_products_found', 'show_products_on_no_products_found', 20 );
+function show_products_on_no_products_found() {
+	echo '<h2>' . __( 'You may be interested in…', 'domain' ) . '</h2>';
+	echo do_shortcode( '[recent_products per_page="4"]' );
+}
